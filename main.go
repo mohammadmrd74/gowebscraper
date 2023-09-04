618cf67f087c326b2c3d26ed6777107bceebd0ed
mohammad moradi <mohammadmrd74@gmail.com>
2023-09-04 17:43:17 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
+func main() {
+
+	c := colly.NewCollector()
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed")
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+
+	writer.Write(headers)
+
+	var pokemonProducts []PokemonProduct
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL)
+	})
+
+	c.OnHTML("li.product", func(h *colly.HTMLElement) {
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = h.ChildAttr("a", "href")
+		pokemonProduct.image = h.ChildAttr("img", "src")
+		pokemonProduct.name = h.ChildText("h2")
+		pokemonProduct.price = h.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(pokemonProducts[0].url)
+	})
+
+	// downloading the target HTML page
+	err := c.Visit("https://scrapeme.live/shop/page/1/")
+
+	if err != nil {
+		log.Printf("failed to visit url: %v\n", err)
+		return
+	}
+}
