9156af14dd6a6732948505c1dcaaa5778ef5966d
mohammad moradi <mohammadmrd74@gmail.com>
2023-09-05 14:21:35 +0330

    main

--- main.go
+++ main.go
@@ -2,76 +2,128 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gocolly/colly"
 )
 
+// defining a data structure to store the scraped data
 type PokemonProduct struct {
 	url, image, name, price string
 }
 
-func main() {
+// it verifies if a string is present in a slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
 
+	return false
+}
+
+func main() {
+	// initializing the slice of structs that will contain the scraped data
+	var pokemonProducts []PokemonProduct
+
+	// initializing the list of pages to scrape with an empty slice
+	var pagesToScrape []string
+
+	// the first pagination URL to scrape
+	pageToScrape := "https://scrapeme.live/shop/page/1/"
+
+	// initializing the list of pages discovered with a pageToScrape
+	pagesDiscovered := []string{pageToScrape}
+
+	// current iteration
+	i := 1
+	// max pages to scrape
+	limit := 5
+
+	// initializing a Colly instance
 	c := colly.NewCollector()
+	// setting a valid User-Agent header
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+
+	// iterating over the list of pagination links to implement the crawling logic
+	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
+		// discovering a new page
+		newPaginationLink := e.Attr("href")
+
+		// if the page discovered is new
+		if !contains(pagesToScrape, newPaginationLink) {
+			// if the page discovered should be scraped
+			if !contains(pagesDiscovered, newPaginationLink) {
+				pagesToScrape = append(pagesToScrape, newPaginationLink)
+			}
+			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
+		}
+	})
+
+	// scraping the product data
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	})
+
+	c.OnScraped(func(response *colly.Response) {
+		// until there is still a page to scrape
+		if len(pagesToScrape) != 0 && i < limit {
+			// getting the current page to scrape and removing it from the list
+			pageToScrape = pagesToScrape[0]
+			pagesToScrape = pagesToScrape[1:]
+
+			// incrementing the iteration counter
+			i++
+
+			// visiting a new page
+			c.Visit(pageToScrape)
+		}
+	})
+
+	// visiting the first page
+	c.Visit(pageToScrape)
+
+	// opening the CSV file
 	file, err := os.Create("products.csv")
 	if err != nil {
-		log.Fatalln("Failed")
+		log.Fatalln("Failed to create output CSV file", err)
 	}
 	defer file.Close()
 
+	// initializing a file writer
 	writer := csv.NewWriter(file)
 
+	// defining the CSV headers
 	headers := []string{
 		"url",
 		"image",
 		"name",
 		"price",
 	}
-
+	// writing the column headers
 	writer.Write(headers)
 
-	var pokemonProducts []PokemonProduct
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
-	})
-
-	c.OnHTML("li.product", func(h *colly.HTMLElement) {
-		pokemonProduct := PokemonProduct{}
-
-		pokemonProduct.url = h.ChildAttr("a", "href")
-		pokemonProduct.image = h.ChildAttr("img", "src")
-		pokemonProduct.name = h.ChildText("h2")
-		pokemonProduct.price = h.ChildText(".price")
-
-		pokemonProducts = append(pokemonProducts, pokemonProduct)
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(pokemonProducts[0].url)
-
-		for _, pokemonProduct := range pokemonProducts {
-			record := []string{
-				pokemonProduct.url,
-				pokemonProduct.image,
-				pokemonProduct.name,
-				pokemonProduct.price,
-			}
-
-			writer.Write(record)
+	// adding each Pokemon product to the CSV output file
+	for _, pokemonProduct := range pokemonProducts {
+		// converting a PokemonProduct to an array of strings
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
 		}
-	})
 
-	defer writer.Flush()
-
-	// downloading the target HTML page
-	err1 := c.Visit("https://scrapeme.live/shop/page/1/")
-
-	if err1 != nil {
-		log.Printf("failed to visit url: %v\n", err)
-		return
+		// writing a new CSV record
+		writer.Write(record)
 	}
+	defer writer.Flush()
 }
